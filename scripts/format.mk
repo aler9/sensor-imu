6d861c64a595200ddf1fc77203ddb407d353735b
aler9 <46489434+aler9@users.noreply.github.com>
2022-08-23 10:13:26 +0200

    split makefile into multiple scripts

---
+++ scripts/format.mk
@@ -0,0 +1,24 @@
+define DOCKERFILE_FORMAT
+FROM ubuntu:20.04
+
+RUN apt update && apt install -y --no-install-recommends \
+	clang-format
+
+ENTRYPOINT [ "sh", "-c", "clang-format \
+	-i \
+	--style=\"{BasedOnStyle: llvm, IndentWidth: 4}\"\
+	*.c \
+	*.h \
+	orientation/*.c \
+	orientation/*.h \
+	test/*.c" ]
+
+endef
+export DOCKERFILE_FORMAT
+
+format:
+	echo "$$DOCKERFILE_FORMAT" | docker build - -t temp
+	docker run --rm \
+	-v $(PWD):/s \
+	-w /s \
+	temp
---
+++ scripts/test.mk
@@ -0,0 +1,2 @@
+test:
+	cd test && make build-cross
